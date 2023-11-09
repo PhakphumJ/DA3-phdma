1e7ebdd880d251c34202ce73c6b554fa5e832e08
PhakphumJ <fphakphum.134@gmail.com>
2023-11-09 01:12:17 +0100

    Filter to get subsample (agri worker)

--- Assignment 1/Assignment1_RCode.R
+++ Assignment 1/Assignment1_RCode.R
@@ -3,7 +3,13 @@
 setwd("D:/Onedrive-CEU/OneDrive - Central European University/CEU/Prediction with Machine Learning/Assignment/DA3-phdma/Assignment 1")
 
 library(readr)
+library(dplyr)
 
 # Importing the data
 
 DataOriginal <- read_csv("morg-2014-emp.csv")
+
+# Filter - only select 'Miscellaneous agricultural workers, including animal breeders' - 6050
+
+MiscWorkerData = DataOriginal %>%
+  filter(occ2012 == 6050)
