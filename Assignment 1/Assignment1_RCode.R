6d6a2d8514da60657e0767b8f185d9ab7a77a9a9
PhakphumJ <fphakphum.134@gmail.com>
2023-11-09 01:07:45 +0100

    Initialize the code and import data

---
+++ Assignment 1/Assignment1_RCode.R
@@ -0,0 +1,9 @@
+## Code for Prediction with Machine Learning _ Assignment 1
+# Set the working directory
+setwd("D:/Onedrive-CEU/OneDrive - Central European University/CEU/Prediction with Machine Learning/Assignment/DA3-phdma/Assignment 1")
+
+library(readr)
+
+# Importing the data
+
+DataOriginal <- read_csv("morg-2014-emp.csv")
